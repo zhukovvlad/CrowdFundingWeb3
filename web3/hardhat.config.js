bbdea00f5d5f7339b87007c8cec820bed694ab80
zhukovvlad <zhukovvlad@yandex.ru>
2023-01-25 00:31:58 +0300

    Deployed smartcontract via thirdweb

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,7 +1,15 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: '0.8.9',
+    version: "0.8.9",
+    defaultNetwork: "goerli",
+    networks: {
+      hardhat: {},
+      goerli: {
+        url: "https://goerli.infura.io/v3/",
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
