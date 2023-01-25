d2acdd344b474333e7efb65ea83b0ae82a7f6188
zhukovvlad <zhukovvlad@yandex.ru>
2023-01-25 14:23:47 +0300

    Created initial structure for client app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
-        <Router>
-            <App />
-        </Router>
-    </ThirdwebProvider>
-)
+  <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+    <Router>
+      <App />
+    </Router>
+  </ThirdwebProvider>
+);
---
+++ client/src/pages/Ptofile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ptofile = () => {
+  return (
+    <div>Ptofile</div>
+  )
+}
+
+export default Ptofile
