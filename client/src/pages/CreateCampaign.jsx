298cc62b0b2653d6ab7deddb89fe62bab3158167
zhukovvlad <zhukovvlad@yandex.ru>
2023-01-28 17:34:17 +0300

    Created header inside Create Campaign Component

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -18,7 +18,22 @@ const CreateCampaign = () => {
     image: "",
   });
 
-  return <div>CreateCampaign</div>;
+  const handleSubmit = () => {};
+
+  return (
+    <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+      {isLoading && "Loader..."}
+      <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]">
+        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">
+          Start a Campaign
+        </h1>
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        className="w-full mt-[65px] flex flex-col gap-[30px]"
+      ></form>
+    </div>
+  );
 };
 
 export default CreateCampaign;
