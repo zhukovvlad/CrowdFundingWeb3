44f75d42d15fd0909570949edd04cf0a6467c657
zhukovvlad <zhukovvlad@yandex.ru>
2023-01-29 20:32:49 +0300

    Created handleSubmit function in CreateCampaign Component

---
+++ client/src/context/index.jsx
@@ -0,0 +1,55 @@
+import React, { useContext, createContext } from "react";
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+import { CreateCampaign } from "../pages";
+import { formatEther } from "ethers/lib/utils";
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract(
+    "0xbaCc8084055FdD9850eD45A3058BdCeEf5856b87"
+  );
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  );
+
+  const address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign([
+        address, //owner
+        form.title, //title
+        form.description, //description
+        form.target,
+        new Date(form.deadline).getTime(), //deadline
+        form.image,
+      ]);
+      console.log("Contract call success", data);
+    } catch (error) {
+      console.log("Contract call failed", error);
+    }
+  };
+
+  return (
+    <StateContext.Provider
+      value={{
+        address,
+        contract,
+        createCampaign: publishCampaign,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ethers } from "ethers";
 
+import { useStateContext } from "../context";
 import { money } from "../assets";
 import { CustomButton, FormField } from "../components";
 import { checkIfImage } from "../utils";
@@ -9,6 +10,7 @@ import { checkIfImage } from "../utils";
 const CreateCampaign = () => {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
+  const { createCampaign } = useStateContext();
   const [form, setForm] = useState({
     name: "",
     title: "",
@@ -22,8 +24,22 @@ const CreateCampaign = () => {
     setForm({ ...form, [fieldName]: e.target.value });
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
+
+    checkIfImage(form.image, async (exists) => {
+      if (exists) {
+        setIsLoading(true);
+        await createCampaign({
+          ...form,
+          target: ethers.utils.parseUnits(form.target, 18),
+        });
+        setIsLoading(false);
+      } else {
+        alert("Provide valid image Url");
+        setForm({ ...form, image: "" });
+      }
+    });
 
     console.log(form);
   };
