491b19a0077905fe7cea45e0f98caba59ac927a8
zhukovvlad <zhukovvlad@yandex.ru>
2023-01-29 20:51:21 +0300

    Added connect function in StateContext Provider

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
+import { useStateContext } from "../context";
 import { CustomButton } from "./";
 import { logo, menu, search, thirdweb } from "../assets";
 import { navlinks } from "../constants";
@@ -9,8 +10,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   const [isActive, setisActive] = useState("dashboard");
   const [toggleDrawer, setToggleDrawer] = useState(false);
-
-  const address = "0xsdcevrcer45trevev";
+  const {connect, address} = useStateContext();
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
