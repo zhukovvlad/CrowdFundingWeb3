cc0caa34c4760a526d48b485249207baf952c3ce
zhukovvlad <zhukovvlad@yandex.ru>
2023-02-02 19:30:31 +0300

    Implemented Creator field with hardcoded quantity of owners campaigns

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -50,6 +50,28 @@ const CampaignDetails = () => {
           <CountBox title="Total Backers" value={donators.length} />
         </div>
       </div>
+
+      <div className="mt-[60px] flex lg:flex-row flex-col gap-5">
+        <div className="flex-[2] flex flex-col gap-[40px]">
+          <div>
+            <h4
+              className="font-epilogue font-semibold text-[18px]
+            text-white p-3 uppercase"
+            >
+              Creator
+            </h4>
+            <div className="mt-[20px] flex flex-row items-center flex-wrap gap-[14px]">
+              <div className="w-[52px] h-[52px] flex items-center justify-center rounded-full bg-[#2c2f32] cursor-pointer">
+                <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain" />
+              </div>
+              <div>
+                <h4 className="font-epilogue font-semibold text-[14px] text-white break-all">{state.owner}</h4>
+                <p className="mt-[4px] font-epilogue font-normal text-[12px] text-[#808191]">10 Campaigns</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
